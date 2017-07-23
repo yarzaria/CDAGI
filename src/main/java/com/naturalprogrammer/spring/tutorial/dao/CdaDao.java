72fbba682f56667cfea389363c6dbe91df7a3ac0
Admin <Admin@Admin-PC>
2017-07-23 10:26:44 +0530

    REST APis expose

--- src/main/java/com/naturalprogrammer/spring/tutorial/dao/CdaDao.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/dao/CdaDao.java
@@ -3,9 +3,11 @@ package com.naturalprogrammer.spring.tutorial.dao;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.naturalprogrammer.spring.tutorial.entities.Cda;
 
+@RestResource(path="cads",rel="cads")
 public interface CdaDao extends CrudRepository<Cda, Long>{
 	
      List<Cda> findAll();
